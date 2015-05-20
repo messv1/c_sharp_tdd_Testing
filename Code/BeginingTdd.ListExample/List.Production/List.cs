c0f3017db1a2adaa584ba274507d433378286f8c
messv1 <vmesserli@gmail.com>
2015-05-20 18:47:03 +0200

    added items are retrievable via their index in List implementation

---
+++ Code/BeginingTdd.ListExample/List.Production/List.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List.Production
+{
+    public class List<T>
+    {
+        private int _count;
+        private T[] _internalArray;
+
+        public List()
+        {
+            _internalArray = new T[8];
+        }
+        public int Count { get { return _count ; } }
+
+        public void Add(T item)
+        {
+            _internalArray[_count] = item;
+            _count++;
+        }
+
+        public T this[int index]
+        {
+            get { return _internalArray[index]; }
+        }
+
+    }
+}
---
+++ Code/BeginingTdd.ListExample/List.Tests/ListTests.cs
@@ -0,0 +1,64 @@
+﻿using List.Production;
+using Xunit;
+using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
+namespace List.Tests
+{
+    public class ListTests
+    {
+        [Theory]
+        [MemberData("CountTestDataForAdd")]
+        public void CountReflectsTheNumberOfItemsThatWereAddedToTheList(int [] itemsToAdd)
+        {
+            /* Arrange Phase
+             * Set up all objects that you need for the test 
+             */
+            var testTarget = new List<int>();
+
+            /* Act Phase
+             * Perform the action that you actually want to test
+             */
+            foreach (var item in itemsToAdd)
+            {
+                testTarget.Add(item);
+            }
+
+            /*Assert Phase
+             *  Test if the outcome oft the action ist correct
+             */
+            Assert.Equal(itemsToAdd.Length, testTarget.Count);
+        }
+
+        public static readonly TestData CountTestDataForAdd = new[] 
+                                                                {
+                                                                    new object[] { new int[] { 1, 2 , 3 , 4 } },
+                                                                    new object[] { new int[] { 33, 22, 44 } },
+                                                                    new object[] { new int[] {15, 100, 179, 63, 62, 12, 88 } }
+                                                                };
+        
+        [Theory]
+        [MemberData("IndexTestDataForAdd")]
+        public void ItemThatAreAddedMustBeRetrievableViaIndex<T>(T[] itemsToBeAdded, 
+                                                                    int index,
+                                                                    T expected)
+        {
+            var testTarget = new List<T>();
+
+            foreach (var item in itemsToBeAdded)
+            {
+                testTarget.Add(item);    
+            }
+
+            Assert.Equal(expected, testTarget[index]);
+        }
+
+        public static readonly TestData IndexTestDataForAdd = new[] {
+            new object[] { new int[] { 1, 2 ,3 ,4 }, 1, 2 },
+            new object[] { new string[] { "Hello", "World" }, 0, "Hello" }
+        };
+    }
+
+    
+   
+
+}
